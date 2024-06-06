4a4403909d12fd3933c3da98b0dcd25bff1ee2a2
levis-creator <47003569+levis-creator@users.noreply.github.com>
2024-06-06 04:18:56 -0700

    Initial commit

---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,33 @@
+import { AlignJustify } from "lucide-react"
+import "./navbar.css"
+
+// Home
+// resume
+// services
+// skills
+// projects
+// about
+// Contact
+// lets talk button
+function Navbar() {
+  return (
+    <nav>
+      <div>Logo</div>
+      <button className="iconbutton"><AlignJustify/> </button>
+      <ul className="navlinks">
+        <li>Home</li>
+        <li>Resume</li>
+        <li>Services</li>
+        <li>Skills</li>
+        <li>Projects</li>
+        <li>About</li>
+        <li>Contact</li>
+        <li><button className="primarybutton">
+          Lets talk
+        </button></li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,19 @@
+function Home(){
+return(
+    <section id="hero-section" className="">
+    <div>
+        {/* intro text */}
+        <div className="intro-text">
+            <h6>Hello</h6>
+            <span>
+            i&apos;m a <h1>Web Designer</h1>
+            </span>
+            <p>A freelancer UI & UX designer</p>
+        </div>
+        {/* image */}
+    </div>
+    </section>
+
+)
+}
+export default Home;
