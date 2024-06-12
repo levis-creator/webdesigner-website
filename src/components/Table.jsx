1becb2183c470fbb147d4b575b7cc2f5f6e8761b
levis-creator <47003569+levis-creator@users.noreply.github.com>
2024-06-12 09:29:42 -0700

    About page

---
+++ src/components/Table.jsx
@@ -0,0 +1,48 @@
+
+function Table() {
+  return (
+    <table>
+    <tbody>
+
+    <tr>
+      <th scope='row'>Name:</th>
+      <td>
+        John Doe
+      </td>
+    </tr>
+    <tr>
+      <th scope='row'>Date of Birth:</th>
+      <td>
+       30th june 1957
+      </td>
+    </tr>
+    <tr>
+      <th scope='row'>Address:</th>
+      <td>
+      Elice tower along Kangundo road
+      </td>
+    </tr>
+    <tr>
+      <th scope='row'>Zip Code:</th>
+      <td>
+      00900
+      </td>
+    </tr>
+    <tr>
+      <th scope='row'>Email:</th>
+      <td>
+      joedoe@email.com
+      </td>
+    </tr>
+    <tr>
+      <th scope='row'>Phone:</th>
+      <td>
+      +25498765432
+      </td>
+    </tr>
+    </tbody>
+    </table>
+  )
+}
+
+export default Table
---
+++ src/pages/About/index.jsx
@@ -0,0 +1,22 @@
+import './about.css'
+import myImage2 from '../../assets/myImage2.png'
+import Table from '../../components/Table'
+function About() {
+  return (
+    <section id="about">
+    <div className='about-container'>
+        {/* image */}
+        <div className='about-image'>
+        <img src={myImage2} alt="My image" />
+        </div>
+        {/* info */}
+        <div className="info-text">
+          <h2>About me</h2>
+         <Table/>
+        </div>
+    </div>
+    </section>
+  )
+}
+
+export default About
