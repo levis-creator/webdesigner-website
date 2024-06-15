3f74b02b35d3186450b4ae89fc854e2bf69ca9af
levis-creator <47003569+levis-creator@users.noreply.github.com>
2024-06-15 03:14:01 -0700

    skills

--- src/components/SkillCard/index.jsx
+++ src/components/SkillCard/index.jsx
@@ -1,10 +1,23 @@
-import { CircularProgressbar } from 'react-circular-progressbar';
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
+import "./skillCard.css"
 function SkillCard() {
   const percentage=66
   return (
     <div className="skill-card">
-    <CircularProgressbar value={percentage} text={``}/>
+    <CircularProgressbar 
+    value={percentage}
+     text={`${percentage}%`}
+     strokeWidth={10}
+     styles={
+      buildStyles(
+        {
+          pathColor:"#f00"
+        }
+      )
+     }
+     />
+     <h3>Figma</h3>
     </div>
   )
 }
